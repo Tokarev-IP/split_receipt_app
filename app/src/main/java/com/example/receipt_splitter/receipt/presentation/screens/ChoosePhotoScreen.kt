8fc2d8d5ee7372f478df0e14539facbd15abc2f8
TokarevIP <tokarevrab@gmail.com>
2025-04-03 19:16:43 +0100

    feat: choose multiple receipt images

--- app/src/main/java/com/example/receipt_splitter/receipt/presentation/screens/ChoosePhotoScreen.kt
+++ app/src/main/java/com/example/receipt_splitter/receipt/presentation/screens/ChoosePhotoScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -27,6 +28,9 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
+import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -40,6 +44,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import coil3.compose.AsyncImage
@@ -50,13 +55,9 @@ import com.example.receipt_splitter.receipt.presentation.ReceiptUiErrorIntent
 import com.example.receipt_splitter.receipt.presentation.ReceiptUiEvent
 import com.example.receipt_splitter.receipt.presentation.ReceiptUiState
 import com.example.receipt_splitter.receipt.presentation.ReceiptViewModel
-import com.google.mlkit.vision.documentscanner.GmsDocumentScanner
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
-import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_PDF
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.SCANNER_MODE_BASE
-import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.SCANNER_MODE_BASE_WITH_FILTER
-import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.SCANNER_MODE_FULL
 import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
 import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
 
@@ -70,16 +71,19 @@ fun ChoosePhotoScreen(
     val uiState by receiptViewModel.getUiStateFlow().collectAsState()
     val uiErrorIntent by receiptViewModel.getUiErrorIntentFlow().collectAsState(null)
 
-    var receiptPhotoUri: Uri? by rememberSaveable { mutableStateOf(null) }
+    var listOfImages: List<Uri> by rememberSaveable { mutableStateOf(emptyList()) }
 
     val choosePhotoLaunch = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickVisualMedia(),
-    ) { newUri: Uri? ->
-        receiptPhotoUri = newUri
+        contract = ActivityResultContracts.PickMultipleVisualMedia(maxItems = DataConstantsReceipt.MAX_AMOUNT_OF_IMAGES),
+    ) { newListOfImages: List<Uri> ->
+        listOfImages = if (newListOfImages.size > DataConstantsReceipt.MAX_AMOUNT_OF_IMAGES)
+            newListOfImages.subList(0, DataConstantsReceipt.MAX_AMOUNT_OF_IMAGES + 1)
+        else
+            newListOfImages
     }
 
     val options = GmsDocumentScannerOptions.Builder()
-        .setGalleryImportAllowed(true)
+        .setGalleryImportAllowed(false)
         .setPageLimit(DataConstantsReceipt.MAX_AMOUNT_OF_IMAGES)
         .setResultFormats(RESULT_FORMAT_JPEG)
         .setScannerMode(SCANNER_MODE_BASE)
@@ -93,16 +97,16 @@ fun ChoosePhotoScreen(
             val result =
                 GmsDocumentScanningResult.fromActivityResultIntent(result.data)
             result?.pages?.let { pages ->
-                for (page in pages) {
-                    receiptPhotoUri = page.imageUri
-                }
+                listOfImages = pages.map { it.imageUri }
             }
         }
     }
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
-        topBar = {}
+        topBar = {
+            TopAppBar(title = { Text(text = stringResource(id = R.string.select_receipt_photo)) })
+        }
     ) { innerPadding ->
 
         when (uiState) {
@@ -116,21 +120,20 @@ fun ChoosePhotoScreen(
             is ReceiptUiState.Show -> {
                 ChoosePhotoView(
                     modifier = modifier.padding(innerPadding),
-                    uri = { receiptPhotoUri },
+                    listOfUri = { listOfImages },
                     onChoosePhotoClicked = {
                         choosePhotoLaunch.launch(
-                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                            PickVisualMediaRequest(
+                                mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly,
+                                maxItems = DataConstantsReceipt.MAX_AMOUNT_OF_IMAGES,
+                            )
                         )
                     },
-                    onClearPhotoClicked = { receiptPhotoUri = null },
+                    onClearPhotoClicked = { listOfImages = emptyList() },
                     onGetReceiptFromImageClicked = {
-                        receiptPhotoUri?.let { photoUri ->
-                            receiptViewModel.setUiEvent(
-                                ReceiptUiEvent.ConvertReceiptFromImage(
-                                    photoUri
-                                )
-                            )
-                        }
+                        receiptViewModel.setUiEvent(
+                            ReceiptUiEvent.ConvertImagesToReceipt(listOfImages = listOfImages)
+                        )
                     },
                     onMakePhotoClicked = {
                         currentActivity?.let { myActivity ->
@@ -167,7 +170,7 @@ fun ChoosePhotoScreen(
 @Composable
 private fun ChoosePhotoView(
     modifier: Modifier = Modifier,
-    uri: () -> Uri?,
+    listOfUri: () -> List<Uri>,
     onChoosePhotoClicked: () -> Unit = {},
     onClearPhotoClicked: () -> Unit = {},
     onGetReceiptFromImageClicked: () -> Unit = {},
@@ -178,13 +181,20 @@ private fun ChoosePhotoView(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        uri()?.let { photoUri ->
-            SplitReceiptView(
-                uri = { photoUri },
-                onClearPhotoClicked = { onClearPhotoClicked() },
-                onGetReceiptFromImageClicked = { onGetReceiptFromImageClicked() },
-            )
-        } ?: run {
+        if (listOfUri().isNotEmpty()) {
+            if (listOfUri().size == 1) {
+                SplitReceiptView(
+                    onGetReceiptFromImageClicked = { onGetReceiptFromImageClicked() },
+                    uri = { listOfUri()[0] },
+                    onClearPhotoClicked = { onClearPhotoClicked() },
+                )
+            } else
+                ImageCarouselView(
+                    listOfUri = { listOfUri() },
+                    onClearPhotoClicked = { onClearPhotoClicked() },
+                    onGetReceiptFromImageClicked = { onGetReceiptFromImageClicked() },
+                )
+        } else {
             ChoosePhotoBoxView(
                 onChoosePhotoClicked = { onChoosePhotoClicked() },
                 onMakePhotoClicked = { onMakePhotoClicked() },
@@ -217,6 +227,68 @@ private fun SplitReceiptView(
 }
 
 @Composable
+private fun ImageCarouselView(
+    modifier: Modifier = Modifier,
+    listOfUri: () -> List<Uri>,
+    onClearPhotoClicked: () -> Unit = {},
+    onGetReceiptFromImageClicked: () -> Unit = {},
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        ImageCarouselBox(
+            listOfUri = { listOfUri() },
+            onClearPhotoClicked = { onClearPhotoClicked() },
+        )
+        Spacer(modifier = modifier.height(40.dp))
+        OutlinedButton(
+            onClick = { onGetReceiptFromImageClicked() }
+        ) {
+            Text(text = stringResource(id = R.string.split_the_receipt))
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ImageCarouselBox(
+    modifier: Modifier = Modifier,
+    listOfUri: () -> List<Uri>,
+    onClearPhotoClicked: () -> Unit = {},
+    height: Dp = 320.dp,
+    preferredItemWidth: Dp = 240.dp,
+) {
+    Box(modifier = modifier.padding(horizontal = 20.dp)) {
+        HorizontalMultiBrowseCarousel(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height)
+                .padding(top = 60.dp),
+            state = rememberCarouselState(itemCount = { listOfUri().size }),
+            preferredItemWidth = preferredItemWidth,
+            itemSpacing = 16.dp,
+        ) { index ->
+            AsyncImage(
+                model = listOfUri()[index],
+                contentDescription = stringResource(R.string.receipt_photo),
+                contentScale = ContentScale.Crop,
+            )
+        }
+        IconButton(
+            modifier = modifier.align(Alignment.TopEnd),
+            onClick = { onClearPhotoClicked() }
+        ) {
+            Icon(
+                Icons.Filled.Clear,
+                contentDescription = stringResource(id = R.string.clear_receipt_photo)
+            )
+        }
+    }
+}
+
+@Composable
 private fun PhotoBoxView(
     modifier: Modifier = Modifier,
     uri: () -> Uri,
@@ -227,9 +299,11 @@ private fun PhotoBoxView(
             .height(320.dp)
             .width(320.dp)
     ) {
+
         AsyncImage(
             modifier = modifier
                 .fillMaxSize()
+                .padding(top = 40.dp)
                 .align(Alignment.Center)
                 .clip(RoundedCornerShape(40.dp)),
             model = uri(),
@@ -265,7 +339,7 @@ private fun ChoosePhotoBoxView(
         ) {
             Text(text = stringResource(id = R.string.select_receipt_photo))
         }
-        Spacer(modifier = modifier.height(40.dp))
+        Spacer(modifier = modifier.height(20.dp))
         OutlinedButton(
             onClick = { onMakePhotoClicked() }
         ) {
@@ -278,6 +352,6 @@ private fun ChoosePhotoBoxView(
 @Preview(showBackground = true)
 private fun ChoosePhotoViewPreview() {
     ChoosePhotoView(
-        uri = { "1234".toUri() },
+        listOfUri = { listOf("1234".toUri(), "1232345".toUri()) },
     )
 }
