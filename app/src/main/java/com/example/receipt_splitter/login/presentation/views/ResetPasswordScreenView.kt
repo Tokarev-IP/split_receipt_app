83e4355d82b419c56bf5aa5d5885dc7bf8df698d
TokarevIP <tokarevrab@gmail.com>
2025-04-21 16:10:23 +0100

    feat: add ResetPasswordScreenView padding

--- app/src/main/java/com/example/receipt_splitter/login/presentation/views/ResetPasswordScreenView.kt
+++ app/src/main/java/com/example/receipt_splitter/login/presentation/views/ResetPasswordScreenView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -56,7 +57,7 @@ private fun ResetPasswordView(
     var emailText by rememberSaveable { mutableStateOf("") }
 
     Column(
-        modifier.fillMaxSize(),
+        modifier.fillMaxSize().padding(40.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
