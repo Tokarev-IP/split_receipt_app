59f55b8d1644a1877196611057f8eb0b16f8bb24
TokarevIP <tokarevrab@gmail.com>
2025-04-21 15:33:30 +0100

    modify Firebase authentication logic

--- app/src/main/java/com/example/receipt_splitter/login/data/FirebaseAuthentication.kt
+++ app/src/main/java/com/example/receipt_splitter/login/data/FirebaseAuthentication.kt
@@ -1,5 +1,6 @@
 package com.example.receipt_splitter.login.data
 
+import com.example.receipt_splitter.login.presentation.LoginUiMessages
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -12,11 +13,41 @@ class FirebaseAuthentication : FirebaseAuthenticationInterface {
 
     private val firebaseAuth = Firebase.auth
 
-    override suspend fun signInWithCredential(credential: AuthCredential): FirebaseUser? {
+    override suspend fun signInWithCredential(credential: AuthCredential): FirebaseUser {
         return suspendCancellableCoroutine { continuation ->
             firebaseAuth.signInWithCredential(credential)
                 .addOnSuccessListener { result ->
-                    continuation.resume(result.user)
+                    continuation.resume(result.user ?: throw Exception(LoginUiMessages.INTERNAL_ERROR.message))
+                }
+                .addOnFailureListener { exception ->
+                    continuation.resumeWithException(exception)
+                }
+        }
+    }
+
+    override suspend fun createUserWithEmailAndPassword(
+        email: String,
+        password: String
+    ): FirebaseUser {
+        return suspendCancellableCoroutine { continuation ->
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener { result ->
+                    continuation.resume(result.user ?: throw Exception(LoginUiMessages.INTERNAL_ERROR.message))
+                }
+                .addOnFailureListener { exception ->
+                    continuation.resumeWithException(exception)
+                }
+        }
+    }
+
+    override suspend fun signInWithEmailAndPassword(
+        email: String,
+        password: String
+    ): FirebaseUser {
+        return suspendCancellableCoroutine { continuation ->
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener { result ->
+                    continuation.resume(result.user ?: throw Exception(LoginUiMessages.INTERNAL_ERROR.message))
                 }
                 .addOnFailureListener { exception ->
                     continuation.resumeWithException(exception)
@@ -28,13 +59,33 @@ class FirebaseAuthentication : FirebaseAuthenticationInterface {
         firebaseAuth.signOut()
     }
 
-    override suspend fun getCurrentUserId(): String? {
-        return firebaseAuth.currentUser?.uid
+    override suspend fun getCurrentUser(): FirebaseUser? {
+        return firebaseAuth.currentUser
+    }
+
+    override suspend fun sendEmailVerification(currentUser: FirebaseUser) {
+        return suspendCancellableCoroutine { continuation ->
+            currentUser.sendEmailVerification()
+                .addOnSuccessListener { continuation.resume(Unit) }
+                .addOnFailureListener { e -> continuation.resumeWithException(e) }
+        }
+    }
+
+    override suspend fun sendPasswordResetEmail(email: String) {
+        return suspendCancellableCoroutine { continuation ->
+            firebaseAuth.sendPasswordResetEmail(email)
+                .addOnSuccessListener { continuation.resume(Unit) }
+                .addOnFailureListener { e -> continuation.resumeWithException(e) }
+        }
     }
 }
 
 interface FirebaseAuthenticationInterface {
-    suspend fun signInWithCredential(credential: AuthCredential): FirebaseUser?
+    suspend fun signInWithCredential(credential: AuthCredential): FirebaseUser
+    suspend fun createUserWithEmailAndPassword(email: String, password: String): FirebaseUser
+    suspend fun signInWithEmailAndPassword(email: String, password: String): FirebaseUser
     suspend fun signOut()
-    suspend fun getCurrentUserId(): String?
+    suspend fun getCurrentUser(): FirebaseUser?
+    suspend fun sendEmailVerification(currentUser: FirebaseUser)
+    suspend fun sendPasswordResetEmail(email: String)
 }
