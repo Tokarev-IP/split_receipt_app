04617ef783b344b9f27c39a196f78c67f0e096de
TokarevIP <tokarevrab@gmail.com>
2025-05-07 12:58:57 +0100

    Refactor navigation and add screens
    -   Refactored the navigation logic within `ReceiptCompose` to use intent flows for navigating between screens.
    -   Added new screens: `AllReceiptsScreen`, `CreateReceiptScreen`, and `EditReceiptScreen`.
    -   Introduced new ViewModels: `AllReceiptsViewModel`, `CreateReceiptViewModel`, and `EditReceiptViewModel`.
    -   Updated the navigation destinations to include `AllReceiptsScreenNav`, `CreateReceiptScreenNav`, and `EditReceiptScreenNav`.
    -   Added navigation to the settings from receipt screen.
    -   Added navigation for pop back stack.

--- app/src/main/java/com/example/receipt_splitter/receipt/presentation/ReceiptCompose.kt
+++ app/src/main/java/com/example/receipt_splitter/receipt/presentation/ReceiptCompose.kt
@@ -3,17 +3,26 @@ package com.example.receipt_splitter.receipt.presentation
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.example.receipt_splitter.main.presentation.MainEvent
 import com.example.receipt_splitter.main.presentation.MainViewModel
-import com.example.receipt_splitter.receipt.presentation.screens.ChoosePhotoScreen
-import com.example.receipt_splitter.receipt.presentation.screens.ShowReceiptsScreen
+import com.example.receipt_splitter.receipt.presentation.screens.AllReceiptsScreen
+import com.example.receipt_splitter.receipt.presentation.screens.CreateReceiptScreen
+import com.example.receipt_splitter.receipt.presentation.screens.EditReceiptScreen
 import com.example.receipt_splitter.receipt.presentation.screens.SplitReceiptScreen
+import com.example.receipt_splitter.receipt.presentation.viewmodels.AllReceiptsEvent
+import com.example.receipt_splitter.receipt.presentation.viewmodels.AllReceiptsViewModel
+import com.example.receipt_splitter.receipt.presentation.viewmodels.CreateReceiptViewModel
+import com.example.receipt_splitter.receipt.presentation.viewmodels.EditReceiptEvent
+import com.example.receipt_splitter.receipt.presentation.viewmodels.EditReceiptViewModel
+import com.example.receipt_splitter.receipt.presentation.viewmodels.SplitReceiptEvent
+import com.example.receipt_splitter.receipt.presentation.viewmodels.SplitReceiptViewModel
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun ReceiptCompose(
@@ -21,31 +30,56 @@ fun ReceiptCompose(
     mainViewModel: MainViewModel,
     receiptViewModel: ReceiptViewModel,
     navHostController: NavHostController = rememberNavController(),
-    startDestination: ReceiptNavHostDestinations = ReceiptNavHostDestinations.ShowReceiptsScreenNav,
+    startDestination: ReceiptNavHostDestinations = ReceiptNavHostDestinations.AllReceiptsScreenNav,
 ) {
-    val intent by receiptViewModel.getIntentFlow().collectAsState(initial = null)
-
     LaunchedEffect(key1 = Unit) {
-        receiptViewModel.setUiEvent(ReceiptEvent.RetrieveAllReceipts)
-    }
+        receiptViewModel.getIntentFlow().collect { receiptIntent ->
+            receiptIntent?.let { intent ->
+                receiptViewModel.clearIntentFlow()
+                when (intent) {
+                    is ReceiptIntent.GoToSplitReceiptScreen -> {
+                        navHostController.navigate(
+                            ReceiptNavHostDestinations.SplitReceiptScreenNav(receiptId = intent.receiptId)
+                        ) {
+                            popUpTo<ReceiptNavHostDestinations.CreateReceiptScreenNav> {
+                                inclusive = false
+                            }
+                            launchSingleTop = true
+                        }
+                    }
 
-    when (intent) {
-        is ReceiptIntent.GoToSplitReceiptScreen -> {
-            receiptViewModel.clearIntentFlow()
-            navHostController.navigate(ReceiptNavHostDestinations.SplitReceiptScreenNav)
-            receiptViewModel.setUiEvent(ReceiptEvent.SetShowState)
-        }
+                    is ReceiptIntent.GoToCreateReceiptScreen -> {
+                        navHostController.navigate(ReceiptNavHostDestinations.CreateReceiptScreenNav) {
+                            launchSingleTop = true
+                        }
+                    }
 
-        is ReceiptIntent.GoToChoosePhotoScreen -> {
-            receiptViewModel.clearIntentFlow()
-            navHostController.navigate(ReceiptNavHostDestinations.ChoosePhotoScreenNav)
-            receiptViewModel.setUiEvent(ReceiptEvent.SetShowState)
-        }
+                    is ReceiptIntent.GoToEditReceiptsScreen -> {
+                        navHostController.navigate(
+                            ReceiptNavHostDestinations.EditReceiptScreenNav(receiptId = intent.receiptId)
+                        ) {
+                            popUpTo<ReceiptNavHostDestinations.CreateReceiptScreenNav> {
+                                inclusive = false
+                            }
+                            launchSingleTop = true
+                        }
+                    }
 
-        is ReceiptIntent.GoToShowReceiptsScreen -> {
-            receiptViewModel.clearIntentFlow()
-            navHostController.navigate(ReceiptNavHostDestinations.ShowReceiptsScreenNav)
-            receiptViewModel.setUiEvent(ReceiptEvent.SetShowState)
+                    is ReceiptIntent.GoToAllReceiptsScreen -> {
+                        navHostController.navigate(ReceiptNavHostDestinations.AllReceiptsScreenNav) {
+                            launchSingleTop = true
+                        }
+                    }
+
+                    is ReceiptIntent.GoBackNavigation -> {
+                        navHostController.popBackStack()
+                    }
+
+                    is ReceiptIntent.GoToSettings -> {
+                        mainViewModel.setEvent(MainEvent.OpenSettings)
+                    }
+                }
+            }
         }
     }
 
@@ -54,16 +88,48 @@ fun ReceiptCompose(
         navController = navHostController,
         startDestination = startDestination,
     ) {
-        composable<ReceiptNavHostDestinations.ChoosePhotoScreenNav> {
-            ChoosePhotoScreen(receiptViewModel = receiptViewModel)
+        composable<ReceiptNavHostDestinations.CreateReceiptScreenNav> {
+            val createReceiptViewModel: CreateReceiptViewModel = koinViewModel()
+            CreateReceiptScreen(
+                receiptViewModel = receiptViewModel,
+                createReceiptViewModel = createReceiptViewModel
+            )
         }
 
-        composable<ReceiptNavHostDestinations.ShowReceiptsScreenNav> {
-            ShowReceiptsScreen(receiptViewModel = receiptViewModel)
+        composable<ReceiptNavHostDestinations.AllReceiptsScreenNav> {
+            val allReceiptsViewModel: AllReceiptsViewModel = koinViewModel()
+            LaunchedEffect(key1 = Unit) {
+                allReceiptsViewModel.setEvent(AllReceiptsEvent.RetrieveAllReceipts)
+            }
+            AllReceiptsScreen(
+                receiptViewModel = receiptViewModel,
+                allReceiptViewModel = allReceiptsViewModel,
+            )
         }
 
-        composable<ReceiptNavHostDestinations.SplitReceiptScreenNav> {
-            SplitReceiptScreen(receiptViewModel = receiptViewModel)
+        composable<ReceiptNavHostDestinations.SplitReceiptScreenNav> { backStackEntry ->
+            val splitReceiptViewModel: SplitReceiptViewModel = koinViewModel()
+            val data = backStackEntry.toRoute<ReceiptNavHostDestinations.EditReceiptScreenNav>()
+            LaunchedEffect(key1 = Unit) {
+                splitReceiptViewModel.setEvent(SplitReceiptEvent.RetrieveReceiptData(data.receiptId))
+                splitReceiptViewModel.setEvent(SplitReceiptEvent.ActivateOrderReportCreator)
+            }
+            SplitReceiptScreen(
+                receiptViewModel = receiptViewModel,
+                splitReceiptViewModel = splitReceiptViewModel,
+            )
+        }
+
+        composable<ReceiptNavHostDestinations.EditReceiptScreenNav> { backStackEntry ->
+            val editReceiptViewModel: EditReceiptViewModel = koinViewModel()
+            val data = backStackEntry.toRoute<ReceiptNavHostDestinations.EditReceiptScreenNav>()
+            LaunchedEffect(key1 = data.receiptId) {
+                editReceiptViewModel.setEvent(EditReceiptEvent.RetrieveReceiptData(data.receiptId))
+            }
+            EditReceiptScreen(
+                receiptViewModel = receiptViewModel,
+                editReceiptViewModel = editReceiptViewModel,
+            )
         }
     }
 }
