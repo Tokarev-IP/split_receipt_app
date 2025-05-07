5a305815122fc4d034f07cbc964bfdcdc56d65a2
TokarevIP <tokarevrab@gmail.com>
2025-05-07 12:59:59 +0100

    Refactor ReceiptDao to support upsert and add more query functions

--- app/src/main/java/com/example/receipt_splitter/receipt/data/room/ReceiptDao.kt
+++ app/src/main/java/com/example/receipt_splitter/receipt/data/room/ReceiptDao.kt
@@ -1,24 +1,60 @@
 package com.example.receipt_splitter.receipt.data.room
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ReceiptDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    //INSERT&UPDATE
+    @Upsert
     suspend fun insertReceipt(receipt: ReceiptEntity): Long
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Upsert
     suspend fun insertOrders(orders: List<OrderEntity>)
+
+    @Upsert
+    suspend fun insertOrder(order: OrderEntity): Long
+
+    //GET
+//    @Transaction
+//    @Query("SELECT * FROM receipt_data")
+//    fun getAllReceiptsWithOrdersFlow(): Flow<List<ReceiptWithOrdersEntity>>
+
+//    @Transaction
+//    @Query("SELECT * FROM receipt_data WHERE id = :receiptId")
+//    fun getReceiptWithOrdersByIdFlow(receiptId: Long): Flow<ReceiptWithOrdersEntity?>
 
     @Transaction
     @Query("SELECT * FROM receipt_data")
-    fun getAllReceiptsWithOrders(): Flow<List<ReceiptWithOrders>>
+    fun getAllReceiptsFlow(): Flow<List<ReceiptEntity>>
 
+    @Transaction
+    @Query("SELECT * FROM receipt_data WHERE id = :receiptId")
+    fun getReceiptByIdFlow(receiptId: Long): Flow<ReceiptEntity?>
+
+    @Transaction
+    @Query("SELECT * FROM receipt_data WHERE id = :receiptId")
+    suspend fun getReceiptById(receiptId: Long): ReceiptEntity?
+
+    @Transaction
+    @Query("SELECT * FROM order_data WHERE receipt_id = :receiptId")
+    fun getOrdersByReceiptIdFlow(receiptId: Long): Flow<List<OrderEntity>>
+
+    @Transaction
+    @Query("SELECT * FROM order_data WHERE receipt_id = :receiptId")
+    suspend fun getOrdersByReceiptId(receiptId: Long): List<OrderEntity>
+
+//    @Transaction
+//    @Query("SELECT * FROM receipt_data WHERE id = :receiptId")
+//    suspend fun getReceiptWithOrdersById(receiptId: Long): ReceiptWithOrdersEntity?
+
+    //DELETE
     @Query("DELETE FROM receipt_data WHERE id = :receiptId")
     suspend fun deleteReceipt(receiptId: Long)
+
+    @Query("DELETE FROM order_data WHERE id = :orderId")
+    suspend fun deleteOrder(orderId: Long)
 }
