1bf5f41dc24848f252d2ecce155e44fc3290ca7f
TokarevIP <tokarevrab@gmail.com>
2025-05-07 12:53:11 +0100

    update Vertex model

--- app/src/main/java/com/example/receipt_splitter/receipt/data/DataConstantsReceipt.kt
+++ app/src/main/java/com/example/receipt_splitter/receipt/data/DataConstantsReceipt.kt
@@ -1,6 +1,6 @@
 package com.example.receipt_splitter.receipt.data
 
 object DataConstantsReceipt {
-    const val VERTEX_AI_MODEL = "gemini-1.5-flash"
+    const val VERTEX_AI_MODEL = "gemini-2.0-flash-lite"
     const val MAX_AMOUNT_OF_IMAGES = 3
 }
