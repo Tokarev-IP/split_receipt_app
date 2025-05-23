4fa618b405de5cf9606d3d2742b2a6f9505568a4
TokarevIP <tokarevrab@gmail.com>
2025-05-23 13:03:09 +0100

    Refactor: Improve UI of `EditReceiptScreenView`
    This commit refactors the `EditReceiptScreenView` to enhance user experience:
    
    - **Expandable Receipt Info:** Receipt details are now collapsible/expandable for better information management.
    - **Improved Layout:** `Row` and `Column` layouts are optimized for clarity.
    - **Removed "Go To Split Screen" Button:** This functionality is likely moved or handled elsewhere.
    - **Animations:** Subtle animations are added for a smoother visual experience.
    - **Updated Previews:** Previews are adjusted to reflect the changes.

--- app/src/main/java/com/example/receipt_splitter/receipt/presentation/views/screens/EditReceiptScreenView.kt
+++ app/src/main/java/com/example/receipt_splitter/receipt/presentation/views/screens/EditReceiptScreenView.kt
@@ -1,9 +1,15 @@
 package com.example.receipt_splitter.receipt.presentation.views.screens
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,14 +22,20 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.KeyboardArrowUp
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -33,83 +45,75 @@ import com.example.receipt_splitter.R
 import com.example.receipt_splitter.main.basic.shimmerBrush
 import com.example.receipt_splitter.receipt.presentation.OrderData
 import com.example.receipt_splitter.receipt.presentation.ReceiptData
-import com.example.receipt_splitter.receipt.presentation.views.OrderItemView
+import com.example.receipt_splitter.receipt.presentation.views.basic.OrderItemView
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun EditReceiptScreenView(
     modifier: Modifier = Modifier,
-    receiptData: () -> ReceiptData?,
-    orderDataList: () -> List<OrderData>,
+    receiptData: ReceiptData?,
+    orderDataList: List<OrderData>,
     onEditOrderClicked: (id: Long) -> Unit,
-    onEditReceiptClicked: () -> Unit,
     onDeleteOrderClicked: (id: Long) -> Unit,
-    onSplitScreenClicked: (receiptId: Long) -> Unit,
-    ordersListState: LazyListState,
+    orderListState: LazyListState,
+    onEditReceiptClicked: () -> Unit,
 ) {
     Box(
         modifier = modifier.fillMaxSize(),
     ) {
-        receiptData()?.let { receipt ->
+        receiptData?.let { receipt ->
             EditReceiptView(
-                receiptData = { receipt },
-                orderDataList = { orderDataList() },
+                receiptData = receipt,
+                orderDataList = orderDataList,
                 onEditOrderClicked = { id -> onEditOrderClicked(id) },
-                onEditReceiptClicked = { onEditReceiptClicked() },
                 onDeleteOrderClicked = { id ->
                     onDeleteOrderClicked(id)
                 },
-                onSplitScreenClicked = { onSplitScreenClicked(receipt.id) },
-                ordersListState = ordersListState,
+                orderListState = orderListState,
+                onEditReceiptClicked = { onEditReceiptClicked() },
             )
         } ?: ShimmedEditReceiptsScreenView()
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun EditReceiptView(
     modifier: Modifier = Modifier,
-    receiptData: () -> ReceiptData = { ReceiptData(id = 0) },
-    orderDataList: () -> List<OrderData> = { emptyList<OrderData>() },
+    receiptData: ReceiptData = ReceiptData(id = 0),
+    orderDataList: List<OrderData> = emptyList<OrderData>(),
     onEditOrderClicked: (id: Long) -> Unit = {},
-    onEditReceiptClicked: () -> Unit = {},
     onDeleteOrderClicked: (id: Long) -> Unit = {},
-    onSplitScreenClicked: () -> Unit = {},
-    ordersListState: LazyListState = rememberLazyListState(),
+    orderListState: LazyListState,
+    onEditReceiptClicked: () -> Unit = {},
 ) {
-    val receiptData = receiptData()
-    val orderDataList = orderDataList()
-
     LazyColumn(
         modifier = modifier
             .fillMaxSize()
             .padding(horizontal = 12.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top,
-        state = ordersListState,
+        state = orderListState,
     ) {
         item {
-            Spacer(modifier = modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
             EditReceiptInfo(
-                receiptData = { receiptData },
-                onEditReceiptClicked = { onEditReceiptClicked() }
+                receiptData = receiptData,
+                onEditReceiptClicked = { onEditReceiptClicked() },
             )
-            Spacer(modifier = modifier.height(8.dp))
-            HorizontalDivider(modifier = modifier.fillMaxWidth())
-            Spacer(modifier = modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
+            HorizontalDivider()
+            Spacer(modifier = Modifier.height(12.dp))
         }
-        items(orderDataList.size) {
-            val orderData = orderDataList[it]
+        items(orderDataList.size) { index ->
+            val orderData = orderDataList[index]
             OrderCardView(
-                orderData = { orderData },
+                orderData = orderData,
                 onDeleteOrderClicked = { onDeleteOrderClicked(orderData.id) },
                 onEditOrderClicked = { onEditOrderClicked(orderData.id) },
             )
-            Spacer(modifier = modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
         }
         item {
-            GoToSplitScreenButtonView(
-                onSplitScreenClicked = { onSplitScreenClicked() }
-            )
+            Spacer(modifier = Modifier.height(60.dp))
         }
     }
 }
@@ -117,128 +121,135 @@ private fun EditReceiptView(
 @Composable
 private fun EditReceiptInfo(
     modifier: Modifier = Modifier,
-    receiptData: () -> ReceiptData,
+    receiptData: ReceiptData,
     onEditReceiptClicked: () -> Unit,
 ) {
-    val receiptData = receiptData()
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
-    Box(
-        modifier = modifier.fillMaxWidth(),
-    ) {
-        EditReceiptInfoView(
-            modifier = modifier.align(Alignment.TopCenter),
-            receiptData = { receiptData }
-        )
-        IconButton(
-            modifier = modifier.align(Alignment.TopEnd),
-            onClick = { onEditReceiptClicked() },
-        ) {
-            Icon(
-                Icons.Outlined.Edit,
-                contentDescription = stringResource(R.string.edit_receipt_button)
-            )
-        }
-    }
-}
-
-@Composable
-private fun EditReceiptInfoView(
-    modifier: Modifier = Modifier,
-    receiptData: () -> ReceiptData,
-) {
-    val receiptData = receiptData()
     Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 40.dp),
+        modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Spacer(modifier = modifier.height(36.dp))
-        OutlinedTextField(
-            value = receiptData.restaurant,
-            onValueChange = {},
-            readOnly = true,
-            label = { Text(text = stringResource(R.string.restaurant_name)) }
-        )
-        Spacer(modifier = modifier.height(8.dp))
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(
+                modifier = modifier.weight(8f).animateContentSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                OutlinedTextField(
+                    value = receiptData.receiptName,
+                    onValueChange = {},
+                    readOnly = true,
+                    label = { Text(text = stringResource(R.string.receipt_name)) }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
 
-        OutlinedTextField(
-            value = receiptData.translatedRestaurant ?: "",
-            onValueChange = {},
-            readOnly = true,
-            label = { Text(text = stringResource(R.string.translated_restaurant_name)) }
-        )
-        Spacer(modifier = modifier.height(8.dp))
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        OutlinedTextField(
+                            value = receiptData.translatedReceiptName ?: "",
+                            onValueChange = {},
+                            readOnly = true,
+                            label = { Text(text = stringResource(R.string.translated_receipt_name)) }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
 
-        OutlinedTextField(
-            value = receiptData.date,
-            onValueChange = {},
-            readOnly = true,
-            label = { Text(text = stringResource(R.string.date)) }
-        )
-        Spacer(modifier = modifier.height(8.dp))
+                        OutlinedTextField(
+                            value = receiptData.date,
+                            onValueChange = {},
+                            readOnly = true,
+                            label = { Text(text = stringResource(R.string.date)) }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
 
-        OutlinedTextField(
-            value = receiptData.total.toString(),
-            onValueChange = {},
-            readOnly = true,
-            label = { Text(text = stringResource(R.string.total)) }
-        )
-        Spacer(modifier = modifier.height(8.dp))
+                        OutlinedTextField(
+                            value = receiptData.total.toString(),
+                            onValueChange = {},
+                            readOnly = true,
+                            label = { Text(text = stringResource(R.string.total)) }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
 
-        OutlinedTextField(
-            value = receiptData.tax?.toString() ?: "",
-            onValueChange = {},
-            readOnly = true,
-            label = { Text(text = stringResource(R.string.tax)) }
-        )
-        Spacer(modifier = modifier.height(8.dp))
+                        OutlinedTextField(
+                            value = receiptData.tax?.toString() ?: "",
+                            onValueChange = {},
+                            readOnly = true,
+                            label = { Text(text = stringResource(R.string.tax)) }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
 
-        OutlinedTextField(
-            value = receiptData.discount?.toString() ?: "",
-            onValueChange = {},
-            readOnly = true,
-            label = { Text(text = stringResource(R.string.discount)) }
-        )
-        Spacer(modifier = modifier.height(8.dp))
+                        OutlinedTextField(
+                            value = receiptData.discount?.toString() ?: "",
+                            onValueChange = {},
+                            readOnly = true,
+                            label = { Text(text = stringResource(R.string.discount)) }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
 
-        OutlinedTextField(
-            value = receiptData.tip?.toString() ?: "",
-            onValueChange = {},
-            readOnly = true,
-            label = { Text(text = stringResource(R.string.tip)) }
-        )
-        Spacer(modifier = modifier.height(8.dp))
+                        OutlinedTextField(
+                            value = receiptData.tip?.toString() ?: "",
+                            onValueChange = {},
+                            readOnly = true,
+                            label = { Text(text = stringResource(R.string.tip)) }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                }
+            }
+
+            IconButton(
+                modifier = modifier.weight(1f),
+                onClick = { onEditReceiptClicked() },
+            ) {
+                Icon(Icons.Outlined.Edit, stringResource(R.string.edit_receipt_info_button))
+            }
+        }
+        IconButton(
+            onClick = { expanded = !expanded }
+        ) {
+            AnimatedContent(targetState = expanded) { expand ->
+                if (expand)
+                    Icon(
+                        modifier = modifier.animateEnterExit(
+                            enter = fadeIn(),
+                            exit = fadeOut(),
+                        ),
+                        imageVector = Icons.Outlined.KeyboardArrowUp,
+                        contentDescription = stringResource(R.string.expand_receipt_info_button),
+                    )
+                else
+                    Icon(
+                        modifier = modifier.animateEnterExit(
+                            enter = fadeIn(),
+                            exit = fadeOut(),
+                        ),
+                        imageVector = Icons.Outlined.KeyboardArrowDown,
+                        contentDescription = stringResource(R.string.narrow_down_receipt_info_button)
+                    )
+            }
+        }
     }
 }
 
 @Composable
 private fun OrderCardView(
     modifier: Modifier = Modifier,
-    orderData: () -> OrderData,
+    orderData: OrderData,
     onDeleteOrderClicked: () -> Unit,
     onEditOrderClicked: () -> Unit,
 ) {
-    val orderData = orderData()
-
     ElevatedCard(
         modifier = modifier.fillMaxWidth(),
     ) {
-        Box(
+        Row(
             modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Column(
-                modifier = modifier
-                    .padding(horizontal = 12.dp)
-                    .align(Alignment.TopCenter),
-            ) {
-                Spacer(modifier = modifier.height(56.dp))
-                OrderItemView(orderData = { orderData })
-                Spacer(modifier = modifier.height(8.dp))
-            }
-
             IconButton(
-                modifier = modifier.align(Alignment.TopStart),
                 onClick = { onDeleteOrderClicked() },
             ) {
                 Icon(
@@ -246,9 +257,7 @@ private fun OrderCardView(
                     contentDescription = stringResource(R.string.delete_order_button)
                 )
             }
-
             IconButton(
-                modifier = modifier.align(Alignment.TopEnd),
                 onClick = { onEditOrderClicked() },
             ) {
                 Icon(
@@ -257,22 +266,14 @@ private fun OrderCardView(
                 )
             }
         }
+        Column(
+            modifier = modifier.padding(horizontal = 12.dp)
+        ) {
+            Spacer(modifier = Modifier.height(8.dp))
+            OrderItemView(orderData = { orderData })
+            Spacer(modifier = Modifier.height(8.dp))
+        }
     }
-}
-
-@Composable
-private fun GoToSplitScreenButtonView(
-    modifier: Modifier = Modifier,
-    onSplitScreenClicked: () -> Unit,
-) {
-    Spacer(modifier = modifier.height(12.dp))
-    Button(
-        modifier = modifier.fillMaxWidth(),
-        onClick = { onSplitScreenClicked() }
-    ) {
-        Text(text = stringResource(R.string.go_to_split_the_receipt))
-    }
-    Spacer(modifier = modifier.height(12.dp))
 }
 
 @Composable
@@ -306,24 +307,23 @@ private fun ShimmedEditReceiptsScreenView(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview(showBackground = true)
 private fun EditReceiptViewPreview() {
     EditReceiptView(
-        receiptData = {
+        receiptData =
             ReceiptData(
                 id = 1,
-                restaurant = "restaurant",
-                translatedRestaurant = "ресторан",
+                receiptName = "restaurant",
+                translatedReceiptName = "ресторан",
                 date = "18/03/2024",
                 total = 60.0f,
                 tax = null,
                 discount = null,
                 tip = null,
-                tipSum = null,
-            )
-        },
-        orderDataList = {
+            ),
+        orderDataList =
             listOf(
                 OrderData(
                     id = 1,
@@ -348,8 +348,8 @@ private fun EditReceiptViewPreview() {
                     price = 30.0f,
                     receiptId = 1,
                 ),
-            )
-        }
+            ),
+        orderListState = rememberLazyListState(),
     )
 }
 
