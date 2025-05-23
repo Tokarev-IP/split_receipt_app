bf05d476c509eae6addf7b16ffbab4a8d050ae7c
TokarevIP <tokarevrab@gmail.com>
2025-05-23 13:08:38 +0100

    Refactor: Move `ImageLabelingKit` to services package
    The `ImageLabelingKit` class has been moved from the `receipt.data` package to the `receipt.data.services` package.

--- app/src/main/java/com/example/receipt_splitter/receipt/data/services/ImageLabelingKit.kt
+++ app/src/main/java/com/example/receipt_splitter/receipt/data/services/ImageLabelingKit.kt
@@ -1,4 +1,4 @@
-package com.example.receipt_splitter.receipt.data
+package com.example.receipt_splitter.receipt.data.services
 
 import android.graphics.Bitmap
 import com.google.mlkit.vision.common.InputImage
