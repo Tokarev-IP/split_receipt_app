39aa2104797da742ad3627398996c384976b9798
TokarevIP <tokarevrab@gmail.com>
2025-05-24 18:57:29 +0100

    Remove ElevatedCard import
    The unused import for `ElevatedCard` has been removed from `CreateReceiptScreenView.kt`.

--- app/src/main/java/com/example/receipt_splitter/receipt/presentation/views/screens/CreateReceiptScreenView.kt
+++ app/src/main/java/com/example/receipt_splitter/receipt/presentation/views/screens/CreateReceiptScreenView.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
