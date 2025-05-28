557920e924fe8efe7df49e68be9bd7afab514f6f
TokarevIP <tokarevrab@gmail.com>
2025-05-28 22:35:14 +0100

    feat: add constants for receipt data processing
    Introduced `DataConstantsReceipt.kt` to store constants related to receipt data extraction and processing. This includes schema definitions for translated and non-translated receipts, Firestore collection and document names, MIME type for responses, appropriate image labels, and other relevant constants.

---
+++ app/src/main/java/com/iliatokarev/receipt_splitter_app/receipt/data/services/DataConstantsReceipt.kt
@@ -0,0 +1,88 @@
+package com.iliatokarev.receipt_splitter_app.receipt.data.services
+
+import com.google.firebase.ai.type.Schema
+
+object DataConstantsReceipt {
+
+    val receiptSchemaObjectTranslated = Schema.obj(
+        mapOf(
+            "receipt_name" to Schema.string(),
+            "translated_receipt_name" to Schema.string(),
+            "date" to Schema.string(),
+            "total_sum" to Schema.float(),
+            "tax_in_percent" to Schema.float(),
+            "discount_in_percent" to Schema.float(),
+            "tip_in_percent" to Schema.float(),
+            "orders" to Schema.array(
+                Schema.obj(
+                    mapOf(
+                        "name" to Schema.string(),
+                        "translated_name" to Schema.string(),
+                        "quantity" to Schema.integer(),
+                        "price" to Schema.float(),
+                    )
+                )
+            )
+        ),
+        optionalProperties = listOf(
+            "receipt_name",
+            "translated_receipt_name",
+            "date",
+            "total_sum",
+            "tax_in_percent",
+            "discount_in_percent",
+            "tip_in_percent",
+            "orders",
+        )
+    )
+
+    val receiptSchemaObjectNotTranslated = Schema.obj(
+        mapOf(
+            "receipt_name" to Schema.string(),
+            "translated_receipt_name" to Schema.string(),
+            "date" to Schema.string(),
+            "total_sum" to Schema.float(),
+            "tax_in_percent" to Schema.float(),
+            "discount_in_percent" to Schema.float(),
+            "tip_in_percent" to Schema.float(),
+            "orders" to Schema.array(
+                Schema.obj(
+                    mapOf(
+                        "name" to Schema.string(),
+                        "translated_name" to Schema.string(),
+                        "quantity" to Schema.integer(),
+                        "price" to Schema.float(),
+                    ),
+                    optionalProperties = listOf(
+                        "translated_name",
+                    )
+                ),
+            )
+        ),
+        optionalProperties = listOf(
+            "receipt_name",
+            "translated_receipt_name",
+            "date",
+            "total_sum",
+            "tax_in_percent",
+            "discount_in_percent",
+            "tip_in_percent",
+            "orders",
+        )
+    )
+
+    const val RESPONSE_MIME_TYPE = "application/json"
+    const val USER_ATTEMPTS_COLLECTION = "amount_of_using"
+    const val MAIN_CONSTANTS_COLLECTION = "main_constants"
+    const val MAIN_CONSTANTS_DOCUMENT = "receipt_constants"
+
+    const val LANGUAGE_TEXT = "Translate to:"
+
+    // Labels for images are the following:
+    // https://developers.google.com/ml-kit/vision/image-labeling/label-map
+    // 135 Menu 240 Receipt 273 Paper 93 Poster
+    val APPROPRIATE_LABELS: List<Int> = listOf(273, 135, 240, 93)
+
+    const val ONE_ATTEMPT = 1
+    const val MAXIMUM_AMOUNT_OF_IMAGES = 2
+}
