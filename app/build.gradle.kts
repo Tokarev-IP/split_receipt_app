962d5d50375aec56fd8bc8fd78dc33f4421039f1
TokarevIP <tokarevrab@gmail.com>
2025-05-28 22:38:46 +0100

    Bump version to 1.5
    Increased the application version code to 5 and version name to "1.5".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.iliatokarev.receipt_splitter_app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.4"
+        versionCode = 5
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
