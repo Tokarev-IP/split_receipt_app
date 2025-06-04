4a696c54e175856f0aea0f4b6b3dcef2572ded1a
TokarevIP <tokarevrab@gmail.com>
2025-06-04 15:07:37 +0100

    Wrap bitmap conversions in `ImageConverter` with `withContext(Dispatchers.Default)`
    The `convertImageFromUriToBitmap` and `convertImageListFromUriToBitmapList` functions in `ImageConverter` now perform bitmap decoding operations on the `Dispatchers.Default` coroutine dispatcher. This prevents potential blocking of the main thread during image processing.

--- app/src/main/java/com/iliatokarev/receipt_splitter_app/receipt/data/services/ImageConverter.kt
+++ app/src/main/java/com/iliatokarev/receipt_splitter_app/receipt/data/services/ImageConverter.kt
@@ -4,22 +4,28 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class ImageConverter(private val appContext: Context) : ImageConverterInterface {
 
     override suspend fun convertImageFromUriToBitmap(imageUri: Uri): Bitmap {
-        val inputStream = appContext.contentResolver.openInputStream(imageUri)
-        val bitmap = BitmapFactory.decodeStream(inputStream)
-        inputStream?.close()
-        return bitmap
-    }
-
-    override suspend fun convertImageListFromUriToBitmapList(listOfImages: List<Uri>): List<Bitmap> {
-        return listOfImages.map {  imageUri ->
+        return withContext(Dispatchers.Default) {
             val inputStream = appContext.contentResolver.openInputStream(imageUri)
             val bitmap = BitmapFactory.decodeStream(inputStream)
             inputStream?.close()
-            bitmap
+            return@withContext bitmap
+        }
+    }
+
+    override suspend fun convertImageListFromUriToBitmapList(listOfImages: List<Uri>): List<Bitmap> {
+        return withContext(Dispatchers.Default) {
+            listOfImages.map { imageUri ->
+                val inputStream = appContext.contentResolver.openInputStream(imageUri)
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                inputStream?.close()
+                bitmap
+            }
         }
     }
 }
