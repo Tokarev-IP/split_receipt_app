703522f6413daa5c1f0ba5391d247af183f4ea12
TokarevIP <tokarevrab@gmail.com>
2025-06-04 15:09:24 +0100

    Refactor: Rename `OrderDataSplitter` to `OrderDataService` and make methods suspendable

--- app/src/main/java/com/iliatokarev/receipt_splitter_app/receipt/domain/OrderDataService.kt
+++ app/src/main/java/com/iliatokarev/receipt_splitter_app/receipt/domain/OrderDataService.kt
@@ -1,15 +1,17 @@
 package com.iliatokarev.receipt_splitter_app.receipt.domain
 
 import com.iliatokarev.receipt_splitter_app.receipt.presentation.OrderData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class OrderDataSplitter() : OrderDataSplitterInterface {
+class OrderDataService() : OrderDataServiceInterface {
 
-    override fun addQuantityToSpecificOrderData(
+    override suspend fun addQuantityToSpecificOrderData(
         orderDataList: List<OrderData>,
         orderId: Long,
-    ): List<OrderData> {
+    ): List<OrderData> = withContext(Dispatchers.Default) {
         runCatching {
-            return orderDataList.map { currentData ->
+            return@withContext orderDataList.map { currentData ->
                 if (currentData.id == orderId && currentData.selectedQuantity < currentData.quantity) {
                     currentData.let {
                         val updatedQuantity = it.selectedQuantity + 1
@@ -20,16 +22,16 @@ class OrderDataSplitter() : OrderDataSplitterInterface {
                 }
             }
         }.getOrElse { e: Throwable ->
-            return orderDataList
+            return@withContext orderDataList
         }
     }
 
-    override fun subtractQuantityToSpecificOrderData(
+    override suspend fun subtractQuantityToSpecificOrderData(
         orderDataList: List<OrderData>,
         orderId: Long,
-    ): List<OrderData> {
+    ): List<OrderData> = withContext(Dispatchers.Default) {
         runCatching {
-            return orderDataList.map { currentData ->
+            return@withContext orderDataList.map { currentData ->
                 if (currentData.id == orderId && currentData.selectedQuantity > 0) {
                     currentData.let {
                         val updatedQuantity = it.selectedQuantity - 1
@@ -40,19 +42,35 @@ class OrderDataSplitter() : OrderDataSplitterInterface {
                 }
             }
         }.getOrElse { e: Throwable ->
-            return orderDataList
+            return@withContext orderDataList
+        }
+    }
+
+    override suspend fun clearAllQuantity(
+        orderDataList: List<OrderData>
+    ): List<OrderData> = withContext(Dispatchers.Default) {
+        runCatching {
+            return@withContext orderDataList.map { currentData ->
+                currentData.copy(selectedQuantity = 0)
+            }
+        }.getOrElse { e: Throwable ->
+            return@withContext orderDataList
         }
     }
 }
 
-interface OrderDataSplitterInterface {
-    fun addQuantityToSpecificOrderData(
+interface OrderDataServiceInterface {
+    suspend fun addQuantityToSpecificOrderData(
         orderDataList: List<OrderData>,
         orderId: Long,
     ): List<OrderData>
 
-    fun subtractQuantityToSpecificOrderData(
+    suspend fun subtractQuantityToSpecificOrderData(
         orderDataList: List<OrderData>,
         orderId: Long,
+    ): List<OrderData>
+
+    suspend fun clearAllQuantity(
+        orderDataList: List<OrderData>,
     ): List<OrderData>
 }
